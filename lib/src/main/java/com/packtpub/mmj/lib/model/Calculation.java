9249172d746c2f5bc84653b2af641a442505b3a3
Julio <julio.cesar.vgs@gmail.com>
2022-02-07 09:13:47 -0300

    First Commit

---
+++ lib/src/main/java/com/packtpub/mmj/lib/model/Calculation.java
@@ -0,0 +1,39 @@
+package com.packtpub.mmj.lib.model;
+
+import java.util.List;
+
+public class Calculation {
+    String function;
+    private List<String> input;
+    private List<String> output;
+
+    public Calculation(String function, List<String> input, List<String> output) {
+        this.function = function;
+        this.input = input;
+        this.output = output;
+    }
+
+    public String getFunction() {
+        return function;
+    }
+
+    public void setFunction(String function) {
+        this.function = function;
+    }
+
+    public List<String> getInput() {
+        return input;
+    }
+
+    public void setInput(List<String> input) {
+        this.input = input;
+    }
+
+    public List<String> getOutput() {
+        return output;
+    }
+
+    public void setOutput(List<String> output) {
+        this.output = output;
+    }
+}
---
+++ lib/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.packtpub.mmj.lib {
+    exports com.packtpub.mmj.lib.model to com.packtpub.mmj.rest;
+    opens com.packtpub.mmj.lib.model;
+}
